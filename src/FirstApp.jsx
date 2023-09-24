520bc4874262dc6e9fe4090f8b9d90b6cb2e79ca
Gabriel Maimó <gabrielmaimorigo@gmail.com>
2023-09-24 15:38:05 +0200

    default props

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,17 +1,24 @@
 import PropTypes from 'prop-types';
 
-export const FirstApp = ({title, subTitle}) => {
+export const FirstApp = ({title, subTitle, name}) => {
 
   return (
     <>
     <h1>{title}</h1>
     {/* <code>{JSON.stringify(newMessage)}</code> */}
     <p> {subTitle}</p>
+    <p>{name}</p>
     </>
   )
 }
 
 FirstApp.propTypes = {
   title: PropTypes.string.isRequired,
-  subTitle: PropTypes.number.isRequired
+  subTitle: PropTypes.number,
+}
+
+FirstApp.defaultProps = {
+  name: "Gabri",
+  subTitle: 34,
+  title: "No hay ningún titulo",
 }
--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,6 @@ import './styles.css';
 
 ReactDOM.createRoot(document. getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp title="Hola" subTitle={123} />
+        <FirstApp />
     </React.StrictMode>
 );
