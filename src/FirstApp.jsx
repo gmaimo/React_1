9622c1d8a4d7212b35c2aa5b168a8e7b4e4dc745
Gabriel Maimó <gabrielmaimorigo@gmail.com>
2023-09-24 15:28:09 +0200

    add proptypes

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,23 +1,17 @@
-
-
-const newMessage = {
-  message: 'Hola mundo',
-  title: 'yo'
-};
-
-const saludo = (a, b) => {
-  return a + b;
-}
+import PropTypes from 'prop-types';
 
 export const FirstApp = ({title, subTitle}) => {
-
 
   return (
     <>
     <h1>{title}</h1>
-    {/* <h1>{saludo(1,2)}</h1> */}
     {/* <code>{JSON.stringify(newMessage)}</code> */}
-    <p> {subTitle+1}</p>
+    <p> {subTitle}</p>
     </>
   )
+}
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.number.isRequired
 }
