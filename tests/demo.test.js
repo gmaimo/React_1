6855f7d235c32f8cb827f1b3ec712dea4b17419f
Gabriel Maimó <gabrielmaimorigo@gmail.com>
2023-10-09 19:20:52 +0200

    add intellisense to jest and title on test

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,15 +1,16 @@
 
+describe('Pruebas en <DemoComponent />', () => { 
+    test('esta prueba no debe fallar', () => {
+        
+        // 1. inicialización
+        const message1 = 'Hola';
+        
+        // 2. estímulo
+        const message2 = message1.trim();
+        
+        // 3. observar comportamiento
+        expect (message1).toBe(message2);
+        
+    });
+});
 
-
-test('esta prueba no debe fallar', () => {
-
-    // 1. inicialización
-    
-
-    // 2. estímulo
-
-
-    // 3. observar comportamiento
-
-
-})
