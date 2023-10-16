97251d801f4cd17734e362dc6a7c0bf2c9c3fb62
Gabriel Maimó <gabrielmaimorigo@gmail.com>
2023-10-16 20:14:43 +0200

    test FirstApp w/ React Testing Library

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-//import { FirstApp } from './FirstApp';
+import { FirstApp } from './FirstApp';
 import { CounterApp } from './CounterApp';
 
 // import App from './HelloWorldApp';
@@ -11,6 +11,7 @@ import './styles.css';
 
 ReactDOM.createRoot(document. getElementById('root')).render(
     <React.StrictMode>
-        <CounterApp value={ 100 } />
+        {/* <CounterApp value={ 100 } /> */}
+        <FirstApp title="Hola, soy Gabri"/>
     </React.StrictMode>
 );
---
+++ tests/FirstApp.test.jsx
@@ -0,0 +1,27 @@
+/** @jest-environment jsdom */
+import { render } from '@testing-library/react';
+import { FirstApp } from '../src/FirstApp';
+
+describe('Pruebas en FirstApp', () => { 
+    
+    test('Debe de hacer match con el snapshot', () => { 
+        
+        const title = 'Hola, soy Gabri';
+        const { container } = render( <FirstApp  title={ title }/> );
+
+        expect( container ).toMatchSnapshot();
+
+     });
+
+     test('Debe mostrar el título en un h1', () => { 
+        
+        const title = 'Hola, soy Gabri';
+        const { container, getByText } = render( <FirstApp  title={ title }/> );
+
+        expect( getByText(title) ).toBeTruthy();
+
+        const h1 = container.querySelector('h1');
+        expect(h1.innerHTML).toContain( title );
+
+      });
+ });
